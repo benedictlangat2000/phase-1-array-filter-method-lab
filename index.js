bc19aa5821d678d086c0f5742f2d37b7df51aa73
BenedictKipkuruiLangat <benedictlangat2000@gmail.com>
2023-07-13 22:41:53 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+// findMatching function
+// findMatching function
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
+  
+  // fuzzyMatch function
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  // matchName function
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  
+
+  
+  // Sample driver data
+const drivers = [
+    { name: "Seth", hometown: "Ngong" },
+    { name: "Daisy", hometown: "Runda" },
+    { name: "Tom", hometown: "Kibra" },
+    { name: "Sandra", hometown: "Juja" }
+  ];
+  
+  console.log(findMatching(drivers, "DAISY")); 
+  console.log(fuzzyMatch(drivers, "Sa")); 
+  console.log(matchName(drivers, "Tom")); 
+  
